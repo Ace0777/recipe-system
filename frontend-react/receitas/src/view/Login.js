383409cc71698a34a4637486d52d39576a1c78c2
nikao8 <nicolasmarquesjf@hotmail.com>
2023-11-07 23:25:12 -0300

    finalizando estilo tela de login

---
+++ frontend-react/receitas/src/view/Login.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import logo from '../img/food_icon.png'
+
+const Login = () => {
+	const [showPassword, setShowPassword] = useState(false);
+
+	const onFinish = (values) => {
+		console.log('Valores enviados:', values);
+	};
+
+	const mostrarSenha = () => {
+		setShowPassword(!showPassword);
+	};
+
+	return (
+		<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+			
+			<Form
+				name="login-form"
+				initialValues={{ remember: true }}
+				onFinish={onFinish}
+				style={{ maxWidth: '300px' }}
+			>
+				<img src={logo} alt="Descrição da imagem" style={{ width: '100px', height: '100px', margin: '0 auto', display: 'block' }} />
+				<p style={{ textAlign: 'center', marginBottom: '20px', fontWeight: 'bold', fontSize: '16px' }}>
+					Para acessar a plataforma de receitas, faça o login preenchendo os campos
+				</p>
+				<Form.Item
+					name="email"
+					rules={[{ required: true, message: 'Por favor, insira seu email!' }]
+					}>
+					<Input
+						prefix={<UserOutlined className="site-form-item-icon" />}
+						placeholder="Endereço de e-mail"
+					/>
+				</Form.Item>
+				<Form.Item
+					name="password"
+					rules={[{ required: true, message: 'Por favor, insira sua senha!' }]
+					}>
+					<Input
+						prefix={<LockOutlined className="site-form-item-icon" />}
+						type={showPassword ? 'text' : 'password'}
+						placeholder="Senha"
+					/>
+				</Form.Item>
+				<Form.Item name="showpass" valuePropName="checked">
+					<Checkbox onClick={mostrarSenha}>Exibir senha</Checkbox>
+				</Form.Item>
+				<Form.Item>
+					<Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+						Entrar
+					</Button>
+				</Form.Item>
+			</Form>
+		</div>
+	);
+};
+
+export default Login;
