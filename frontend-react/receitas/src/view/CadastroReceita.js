4a51111588e3b65976700efc29823023afcce95d
Ace0777 <luishhasantos@gmail.com>
2023-11-10 21:33:40 -0300

    Configurando tela de cadastro receita e linkando a mesma com o endpoint de cadastro

--- frontend-react/receitas/src/view/CadastroReceita.js
+++ frontend-react/receitas/src/view/CadastroReceita.js
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
-import { Form, Input, Button, Row, Col, Upload, message } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Form, Input, Button, Row, Col, Upload, message, Select, notification } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+const { Option } = Select;
 
 const CadastroReceita = () => {
   const [nome, setNome] = useState('');
   const [descricao, setDescricao] = useState('');
   const [ingredientes, setIngredientes] = useState([]);
   const [novoIngrediente, setNovoIngrediente] = useState('');
-  const [fotoReceita, setFotoReceita] = useState(null);
+  const [ingredientesSelecionadosEdit, setIngredientesSelecionadosEdit] = useState([]);
+  const [ingredientesSelecionadosInts, setIngredientesSelecionadosInts] = useState([]);
 
   const handleNomeChange = (e) => {
     setNome(e.target.value);
@@ -29,20 +32,67 @@ const CadastroReceita = () => {
     }
   };
 
-  const handleFotoChange = (info) => {
-    if (info.file.status === 'done') {
-      message.success(`${info.file.name} foi enviado com sucesso.`);
-      setFotoReceita(info.file.originFileObj);
-    } else if (info.file.status === 'error') {
-      message.error(`${info.file.name} falhou ao enviar.`);
+  const buscaTodosIngredientes = async () => {
+    let url = `https://localhost:7007/api/ingrediente`;
+
+    try {
+      const response = await axios.get(url);
+      setIngredientesSelecionadosEdit(response.data.$values);
+    } catch (error) {
+      console.error('Falha na requisição:', error);
     }
   };
+
+  useEffect(() => {
+    buscaTodosIngredientes();
+  }, []);
+
+
+  const handleChange = (values) => {
+    setIngredientesSelecionadosInts(values)
+    console.log(ingredientesSelecionadosInts)
+  };
+
+
+  const onFinish = async (values) => {
+    console.log('Valores do formulário:', values);
+    const url = 'https://localhost:7007/api/receita';
+    const data = {
+      nome: nome,
+      descricao: descricao,
+      usuarioId: 1,
+      ingredientesIds: ingredientesSelecionadosInts
+    };
+
+    try {
+      const response = await axios.post(url, data);
+      console.log('Requisição POST bem-sucedida');
+      console.log('Resposta do servidor:', response.data);
+
+
+      notification.success({
+        message: 'Cadastro realizado com sucesso!',
+        description: 'Receita cadastrada com sucesso.',
+      });
+
+
+      setTimeout(() => {
+
+        window.location.href = '/principal';
+      }, 2000);
+
+    } catch (error) {
+      console.error('Falha na requisição:', error);
+    }
+  };
+
+
 
   return (
     <Row justify="center" align="middle" style={{ minHeight: '100vh' }}>
       <Col span={8}>
         <h2 style={{ textAlign: 'center', fontSize: '24px' }}>Cadastro de Receita</h2>
-        <Form>
+        <Form name="cadastro">
           <Form.Item label="Nome">
             <Input value={nome} onChange={handleNomeChange} />
           </Form.Item>
@@ -55,55 +105,34 @@ const CadastroReceita = () => {
             />
           </Form.Item>
 
-          <Form.Item label="Ingredientes">
-            {ingredientes.map((ingrediente, index) => (
-              <div key={index}>{ingrediente}</div>
-            ))}
-            <Input
-              value={novoIngrediente}
-              onChange={handleIngredienteChange}
-            />
-            <Button
-              icon={<PlusOutlined />}
-              type="default"
-              onClick={handleAdicionarIngrediente}
-              style={{ marginTop: '10px' }}
+          <Form.Item
+            label="Ingredientes"
+            
+
+          >
+            <Select
+              mode="multiple"
+              style={{ width: '100%' }}
+              placeholder="Selecione"
+              onChange={handleChange}
             >
-              Adicionar Ingrediente
-            </Button>
+              {ingredientesSelecionadosEdit.map((ingrediente) => (
+                <Option key={ingrediente.nome} value={ingrediente.id}>
+                  {ingrediente.nome}
+                </Option>
+              ))}
+            </Select>
           </Form.Item>
 
-          <Form.Item label="Foto da Receita">
+          <Form.Item label="">
             <Row gutter={16} align="middle">
-              <Col span={12}>
-                <Upload
-                  name="fotoReceita"
-                  listType="picture-card"
-                  showUploadList={false}
-                  beforeUpload={() => false}
-                  onChange={handleFotoChange}
-                >
-                  {fotoReceita ? (
-                    <img
-                      src={URL.createObjectURL(fotoReceita)}
-                      alt="Foto da Receita"
-                      style={{ width: '100%' }}
-                    />
-                  ) : (
-                    <div>
-                      <PlusOutlined />
-                      <div style={{ marginTop: 8 }}>Fazer Upload</div>
-                    </div>
-                  )}
-                </Upload>
-              </Col>
               <Col span={12} style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-                <Button type="primary" htmlType="submit" style={{ marginTop: '10px' }}>
+                <Button type="primary" onClick={onFinish} style={{ marginTop: '10px' }}>
                   Cadastrar Receita
                 </Button>
-                <Button type="primary" style={{marginTop: "10px"}}>
-              <Link to="/principal">Voltar</Link>
-              </Button>
+                <Button type="primary" style={{ marginTop: "10px" }}>
+                  <Link to="/principal">Voltar</Link>
+                </Button>
               </Col>
             </Row>
           </Form.Item>
